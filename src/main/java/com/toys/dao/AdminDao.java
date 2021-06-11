f4465c31c557bdc6f109da1b4a7541e57a354011
yd0nh1sgc <wenpoyan19550824@126.com>
2021-06-11 17:33:11 +0800

    修改Application

---
+++ src/main/java/com/toys/dao/AdminDao.java
@@ -0,0 +1,8 @@
+package com.toys.dao;
+
+/**
+ * Created by Administrator on 2018/1/26.
+ */
+public interface AdminDao {
+
+}
---
+++ src/main/java/com/toys/dao/impl/AdminDaoImpl.java
@@ -0,0 +1,7 @@
+package com.toys.dao.impl;
+
+/**
+ * Created by Administrator on 2018/1/26.
+ */
+public class AdminDaoImpl {
+}
